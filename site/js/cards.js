bb7f53625b6c920e6434f4d7db4d64508030a06c
Noah <noahshomette@gmail.com>
2025-02-16 23:14:15 -0500

    finalize for submission

--- site/js/cards.js
+++ site/js/cards.js
@@ -82,52 +82,98 @@ function renderCard(drug) {
     for (let i = 0; i < drug.class.length; i++) {
         drugCardClone.querySelector('#class').append(createElementFromText(drug.class[i]));
     }
-    for (let i = 0; i < drug.indications.length; i++) {
-        drugCardClone.querySelector('#indications').append(createElementFromText(drug.indications[i]));
-    }
-    for (let i = 0; i < drug.contraindications.length; i++) {
-        drugCardClone.querySelector('#contraindications').append(createElementFromText(drug.contraindications[i]));
-    }
-    for (let i = 0; i < drug.side_effects.length; i++) {
-        drugCardClone.querySelector('#side-effects').append(createElementFromText(drug.side_effects[i]));
-    }
-    for (let i = 0; i < drug.routes.length; i++) {
-        drugCardClone.querySelector('#routes').append(createElementFromText(drug.routes[i]));
+    drugCardClone.querySelector('#indications').append(makeIndicationsLike(drug.indications));
+    drugCardClone.querySelector('#contraindications').append(makeIndicationsLike(drug.contraindications));
+    drugCardClone.querySelector('#side-effects').append(makeIndicationsLike(drug.side_effects));
+    if (drug.routes.length > 0) {
+        for (let i = 0; i < drug.routes.length; i++) {
+            drugCardClone.querySelector('#routes').append(createElementFromText(drug.routes[i]));
+        }
+    } else {
+        drugCardClone.querySelector('#routes-holder').classList.add("d-none");
     }
     if (drug.hasOwnProperty("interactions")) {
         drugCardClone.querySelector('#interactions').innerText = drug.interactions;
+    } else {
+        drugCardClone.querySelector('#interactions-holder').classList.add("d-none");
     }
     if (drug.dose.hasOwnProperty("general")) {
-        for (let i = 0; i < drug.dose.general.length; i++) {
-            drugCardClone.querySelector('#dose-general').append(createElementFromText(drug.dose.general[i]));
+        if (drug.dose.general.length > 0) {
+            for (let i = 0; i < drug.dose.general.length; i++) {
+                drugCardClone.querySelector('#dose-general').append(createElementFromText(drug.dose.general[i]));
+            }
+        } else {
+            drugCardClone.querySelector('#dose-general-holder').classList.add("d-none");
         }
+    } else {
+        drugCardClone.querySelector('#dose-general-holder').classList.add("d-none");
     }
     if (drug.dose.hasOwnProperty("pedi")) {
-        for (let i = 0; i < drug.dose.pedi.length; i++) {
-            drugCardClone.querySelector('#dose-pedi').append(createElementFromText(drug.dose.pedi[i]));
+        if (drug.dose.pedi.length > 0) {
+
+            for (let i = 0; i < drug.dose.pedi.length; i++) {
+                drugCardClone.querySelector('#dose-pedi').append(createElementFromText(drug.dose.pedi[i]));
+            }
+        } else {
+            drugCardClone.querySelector('#dose-pedi-holder').classList.add("d-none");
         }
+    } else {
+        drugCardClone.querySelector('#dose-pedi-holder').classList.add("d-none");
     }
     if (drug.dose.hasOwnProperty("adult")) {
-        for (let i = 0; i < drug.dose.adult.length; i++) {
-            drugCardClone.querySelector('#dose-adult').append(createElementFromText(drug.dose.adult[i]));
+        if (drug.dose.adult.length > 0) {
+
+            for (let i = 0; i < drug.dose.adult.length; i++) {
+                drugCardClone.querySelector('#dose-adult').append(createElementFromText(drug.dose.adult[i]));
+            }
+        } else {
+            drugCardClone.querySelector('#dose-adult-holder').classList.add("d-none");
         }
+    } else {
+        drugCardClone.querySelector('#dose-adult-holder').classList.add("d-none");
     }
+
     if (drug.hasOwnProperty("onset")) {
         drugCardClone.querySelector('#onset').innerText = drug.onset;
+    } else {
+        drugCardClone.querySelector('#onset').classList.add("d-none");
     }
+
     if (drug.hasOwnProperty("duration")) {
         drugCardClone.querySelector('#duration').innerText = drug.duration;
+    } else {
+        drugCardClone.querySelector('#duration').classList.add("d-none");
     }
 
-    drugCardClone.querySelector('#protocols').innerText = drug.protocols;
+    if (drug.hasOwnProperty("protocols")) {
+        if (drug.protocols.length > 0) {
+            for (let i = 0; i < drug.protocols.length; i++) {
+                drugCardClone.querySelector('#protocols').append(createElementFromText(drug.protocols[i]));
+            }
+        } else {
+            drugCardClone.querySelector('#protocols-holder').classList.add("d-none");
+        }
+    } else {
+        drugCardClone.querySelector('#protocols-holder').classList.add("d-none");
+    }
 
     if (drug.hasOwnProperty("pearls")) {
-        for (let i = 0; i < drug.pearls.length; i++) {
-            drugCardClone.querySelector('#pearls').append(createElementFromText(drug.pearls[i]));
+        if (drug.pearls.length > 0) {
+
+            for (let i = 0; i < drug.pearls.length; i++) {
+                drugCardClone.querySelector('#pearls').append(createElementFromText(drug.pearls[i]));
+            }
+        } else {
+            drugCardClone.querySelector('#pearls-holder').classList.add("d-none");
         }
+    } else {
+        drugCardClone.querySelector('#pearls-holder').classList.add("d-none");
     }
+
     if (drug.hasOwnProperty("overdose_signs")) {
         drugCardClone.querySelector('#overdose-signs').innerText = drug.overdose_signs;
+    } else {
+        drugCardClone.querySelector('#overdose-holder').classList.add("d-none");
     }
     drugCards.append(drugCardClone);
 }
@@ -140,6 +186,8 @@ function removeRenderedCard(drug) {
     removeActiveCard(safeDrugName(drug));
 }
 
+
+
 function createElementFromText(text) {
     let newDiv = document.createElement("div");
     newDiv.innerText = text;
@@ -150,6 +198,26 @@ function createElementFromTextWithTemplate(text, template_id) {
     let templateClone = template.content.firstElementChild.cloneNode(true);
     templateClone.innerText = text;
     return templateClone
+}
+
+function createElementFromTextTest(text) {
+
+    var protocolRegex = /\[([^\]]+)\]/g;
+    var routeRegex = /\{([^\}]+)\}/g;
+    let newElement = document.createElement("div");
+
+    var match;
+    while (match = protocolRegex.exec(text)) {
+        let newDiv = document.createElement("div");
+        let split = splitAt(match.index, text, match[0].length);
+        if (split[0].length > 0) {
+            newDiv.innerText = split[0];
+        }
+        newElement.append(newDiv);
+        console.log(split);
+    }
+    return newElement
+
 }
 
 function getActiveCards() {
@@ -179,3 +247,16 @@ function sortRenderedCards() {
 
     sortedNamedElements.forEach((el, index) => el.element.style.order = index)
 }
+
+function makeIndicationsLike(stringArray) {
+    let newElement = document.createElement("ul");
+    for (let i = 0; i < stringArray.length; i++) {
+        let newDiv = document.createElement("li");
+        newDiv.innerText = stringArray[i];
+        newElement.append(newDiv);
+    }
+
+    return newElement;
+}
+
+const splitAt = (index, string, offset) => [string.slice(0, index), string.slice(index + offset, string.length)]
